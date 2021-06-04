1699450166d40ee5ebf4154d3754337691e23210
cqnhanit <cqnhan.it@gmail.com>
2021-06-04 14:53:04 +0700

    update

--- js/main.js
+++ js/main.js
@@ -7,4 +7,26 @@ window.addEventListener('scroll', (e) => {
         document.getElementById('header_contact').classList.remove('d-none');
         document.getElementById('header').classList.remove('bg-black');
     }
+
+    if( window.scrollY > window.innerHeight) {
+        document.getElementById('btnBackToTop').classList.remove('d-none');
+    } else {
+        document.getElementById('btnBackToTop').classList.add('d-none');
+    }
 });
+
+function backToTop() {
+    window.scrollTo({top: 0, behavior:'smooth'})
+}
+
+function onClickSettings() {
+    document.getElementById('settingBlock').classList.remove('d-none')
+}
+
+function onChangeTheme(theme) {
+    if(theme) {
+        console.log('theme', theme)
+        document.getElementsByTagName('body')[0].classList = [theme];
+        document.getElementById('settingBlock').classList.add('d-none')
+    }
+}
